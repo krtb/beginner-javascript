f21f2e5f72a6592dce247a4d044dddba3b87a4b3
Kurt Bauer <kurtbauer55@protonmail.com>
2020-04-14 10:55:27 -0400

    loop over buttons, add handler function on click

--- exercises/34 - Click Outside/click-outside.js
+++ exercises/34 - Click Outside/click-outside.js
@@ -1,1 +1,13 @@
+// select all buttons
 const cardButtons = document.querySelectorAll('.card button');
+
+// handler callback function on card button click
+function handleClick() {
+    console.log('clicked it');
+}
+
+
+// loop over buttons, listen for a click on each of them
+cardButtons.forEach((butt)=> {
+    button.addEventListener('click', handleClick)
+})
