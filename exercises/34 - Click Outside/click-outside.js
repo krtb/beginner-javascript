d55e365e198101a20558ced38eb9ec24a4714b5e
Kurt Bauer <kurtbauer55@protonmail.com>
2020-04-14 11:04:31 -0400

    capture card and button elements in handleClick

--- exercises/34 - Click Outside/click-outside.js
+++ exercises/34 - Click Outside/click-outside.js
@@ -2,12 +2,16 @@
 const cardButtons = document.querySelectorAll('.card button');
 
 // handler callback function on card button click
-function handleClick() {
-    console.log('clicked it');
+function handleClick(event) {
+    // find currently clicked item
+    const button = event.currentTarget;
+    // find closest element with class of card
+    const card = button.closest('.card');
+    
 }
 
 
 // loop over buttons, listen for a click on each of them
-cardButtons.forEach((butt)=> {
-    button.addEventListener('click', handleClick)
-})
+cardButtons.forEach((button)=> {
+    button.addEventListener('click', handleClick);
+});
